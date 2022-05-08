42a9ae3c262a2f5c62c4e690621555ab3ee28527
sanjith1999 <shansanjithofficial@gmail.com>
2022-05-08 12:31:18 +0530

    Assignment 2 : version 1.1

--- Fitting and Alignment/Latex/sections/code_listing.py
+++ Fitting and Alignment/Latex/sections/code_listing.py
@@ -1,21 +1,6 @@
-# Q2
-# Function to create overlay
-def overlay_image(im_src, im_dst, pts_src, pts_dst):
-    h = cv.getPerspectiveTransform(pts_src, pts_dst)
-    transformed_image = cv.warpPerspective(
-        im_src, h, (im_dst.shape[1], im_dst.shape[0]))
-    b = (transformed_image[:, :, 0] == 0)*im_dst[:, :, 0]
-    g = (transformed_image[:, :, 1] == 0)*im_dst[:, :, 1]
-    r = (transformed_image[:, :, 2] == 0)*im_dst[:, :, 2]
-    masked = np.dstack((b, g, r))
-    overlayed = cv.add(masked, transformed_image)
-    return overlayed
-
-
 # Randy Bullock Fit
 def randy_bullock_fit(points):
-    N = len(points)
-    X, Y = points[:, 0], points[:, 1]
+    N, X, Y = N = len(points), points[:, 0], points[:, 1]
     x_bar, y_bar = np.mean(X), np.mean(Y)
     U, V = X - x_bar, Y - y_bar
     s_uu, s_uv, s_vv = np.sum(U * U), np.sum(U * V), np.sum(V * V)
@@ -25,25 +10,13 @@ def randy_bullock_fit(points):
     B = np.array([[1 / 2 * (s_uuu + s_uvv)], [1 / 2 * (s_vvv + s_vuu)]])
     [[u_c], [v_c]] = np.linalg.inv(A) @ B
     alpha = math.pow(u_c, 2) + math.pow(v_c, 2) + (s_uu + s_vv) / N
-    r = math.sqrt(alpha)
-    c_x, c_y = u_c + x_bar, v_c + y_bar
+    r, c_x, c_y = sqrt(alpha), u_c + x_bar, v_c + y_bar
     circ = Circle(c_x, c_y, r)
     return circ
 
 
 # Calculation of Homography
-def calcHomography(m_points):
-    """
-    The normalized DLT for 2D homographs.
-    """
-    p1, p2 = m_points[:, 0], m_points[:, 1]
-    # Normalizing the points using predefined function
-    T1, p1 = Homography.normalizePoints(p1)
-    T2, p2 = Homography.normalizePoints(p2)
-    # Initialising an array to keep the coefficient matrix
-    A = np.zeros((2 * len(p1), 9))
-    row = 0
-    # Filling rows of the matrix according to the expressions
+def calcHomography(point1,point2):
     for point1, point2 in zip(p1, p2):
         A[row, 3:6] = -point2[2] * point1
         A[row, 6:9] = point2[1] * point1
@@ -52,11 +25,7 @@ def calcHomography(m_points):
         row += 2
     # Singular Value decomposition of A
     U, D, VT = np.linalg.svd(A)
-    h = VT[-1]
-    # Reshaping to get 3x3 homography
-    H = h.reshape((3, 3))
-    # Renormalization
-    H = np.linalg.inv(T2).dot(H).dot(T1)
+    h = VT[-1].reshape(3,3)
     return Homography(H)
 
 
